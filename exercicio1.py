364d682c8fd287bb7d21e3c035411e015c1db60c
unknown <jemanuel.orlando@gmail.com>
2026-02-24 09:42:01 -0300

    Realizando exercicios básicos

---
+++ exercicio1.py
@@ -0,0 +1,5 @@
+numero = int(input("Digite um numero: "))
+if numero > 100:
+    print(numero/2)
+else:
+    print(numero)
---
+++ exercicio3.py
@@ -0,0 +1,11 @@
+n1 = int(input("Digite o numero 1: "))
+n2 = int(input("Digite o numero 2: "))
+op = input("Digite a operação (*, /, +, -)")
+if op == "+":
+    print(n1 + n2)
+elif op == "-":
+    print(n1 - n2)
+elif op == "*":
+    print(n1 * n2)
+elif op == "/":
+    print(n1 / n2)
