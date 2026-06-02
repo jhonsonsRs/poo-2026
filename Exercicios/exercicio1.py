d3cb8c190db23ee977e5db8957f94e7ab53763ce
jhonsonsRs <jemanuel.orlando@gmail.com>
2026-06-01 21:03:47 -0300

    Organizando códigos

--- Exercicios/exercicio1.py
+++
@@ -1,5 +0,0 @@
-numero = int(input("Digite um numero: "))
-if numero > 100:
-    print(numero/2)
-else:
-    print(numero)
--- teste.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-int main(){
-    std::string b2 = "oi";
-    const char a1* {"Hello World"}
-    std::cout << b2;
-    std::cout << a1;
-}
